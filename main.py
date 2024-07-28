9a24df9bd19d490c6ce7985c6b6d4aba7615560c
UNDERDROID <roshan.stha399@gmail.com>
2024-07-28 23:25:38 +0545

    GUI color changes

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ def move_files(path, files, selected_folders):
     for file in files:
         if 'Images' in selected_folders and file.endswith(('.jpg', '.png', '.jpeg')) and not os.path.exists(os.path.join(path, 'Images', file)):
             shutil.move(os.path.join(path, file), os.path.join(path, 'Images', file))
-            show_operations(f"{file} was moved to Images folder")
+            show_operations(f"•{file} was moved to Images folder")
             print(f"{file} was moved to Images folder")
 
         elif 'Compressed' in selected_folders and file.endswith('.zip') and not os.path.exists(os.path.join(path, 'Compressed', file)):
             shutil.move(os.path.join(path, file), os.path.join(path, 'Compressed', file))
-            show_operations(f"{file} was moved to compressed folder")
+            show_operations(f"•{file} was moved to compressed folder")
             print(f"{file} was moved to Compressed folder")
         
         else:
-            show_operations(f"No matching folder for {file}")
+            show_operations(f"•No matching folder for {file}")
             print(f"No matching folder for {file}")
 
 # Function to select the path directory and organize the files by calling create_folders() & move_files()
@@ -52,15 +52,18 @@ def organize_files():
     files = os.listdir(path)
     create_folders(path, selected_folders)
     move_files(path, files, selected_folders)
-    messagebox.showinfo("Task Completed", "Files have been organized successfully.")
+    # messagebox.showinfo("Task Completed", "Files have been organized successfully.")
 
 # Create the main window for GUI
 root = tk.Tk()
 root.title("File Master")
 
+# Change the background color of the main window
+root.configure(bg = '#1c1c1c')
+
 # Display Heading
 heading_font = font.Font(family='Helvetica', size=16, weight='bold')
-heading_label = tk.Label(root, text="Select the files you want to organize", font=heading_font)
+heading_label = tk.Label(root, text="Select the files you want to organize", font=heading_font, bg='#1c1c1c', fg='white')
 heading_label.pack(pady=10)
 
 options = ['Images', 'Compressed', 'Video']
@@ -68,17 +71,19 @@ variables = []
 
 for option in options:
     var = tk.BooleanVar()
-    chk = tk.Checkbutton(root, text=option, variable=var)
+    chk = tk.Checkbutton(root, text=option, variable=var, bg='#1c1c1c', fg='white', selectcolor='black', 
+        activebackground='#1c1c1c', activeforeground='white')
     chk.pack(anchor=tk.W)
     variables.append(var)
 
 # Create and place the button
-organize_button = tk.Button(root, text="Organize", command=organize_files)
+organize_button = tk.Button(root, text="Organize", command=organize_files, bg='#383838', fg='white')
 organize_button.pack(pady=50)
 
 # Status text widget
-status_text = tk.Text(root, height=10, width=50)
-status_text.pack(pady=20)
+status_font = font.Font(family='Helvetica', size=8)
+status_text = tk.Text(root, height=15, width=80, font=status_font, bg='#383838', fg='yellow')
+status_text.pack(pady=0)
 
 # Run the application
 root.mainloop()
